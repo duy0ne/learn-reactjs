d7f7ab3195a1a400881f8938b8c6d2c6d071c547
DUYTRAN <duygt2.nd@gmail.com>
2021-12-21 23:54:35 +0700

    Apply material ui

--- src/pages/features/Features.js
+++ src/pages/features/Features.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import SideBar from './sideBar/SideBar';
 import { Outlet } from 'react-router-dom';
-
+import { Grid } from '@mui/material';
 Features.propTypes = {
 
 };
 
 function Features(props) {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-4">
-          <SideBar></SideBar>
-        </div>
-        <div className="col-8">
-          <Outlet />
-        </div>
-      </div>
-    </div>
+    <Grid container>
+      <Grid lg={4}>
+        <SideBar></SideBar>
+      </Grid>
+      <Grid lg={8}>
+        <Outlet />
+      </Grid>
+    </Grid>
   );
 }
 
